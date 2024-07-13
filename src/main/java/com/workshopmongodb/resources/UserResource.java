49ef616a0de9f19f5edd3d167bb685e1b96405da
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-07-12 21:34:25 -0300

    user delete with method delete

--- src/main/java/com/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/workshopmongodb/resources/UserResource.java
@@ -41,5 +41,11 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
 
--- src/main/java/com/workshopmongodb/service/UserService.java
+++ src/main/java/com/workshopmongodb/service/UserService.java
@@ -32,6 +32,11 @@ public class UserService {
         return userRepository.insert(user);
     }
 
+    public void deleteById(String id) {
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
     public User fromDTO(UserDTO userDTO) {
         return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
